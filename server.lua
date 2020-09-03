9d2d6345978af5dcca8f60b3c533286265501a87
Bruno Mussoi Mendonça <bruno_mussoi@hotmail.com>
2020-09-02 21:20:55 -0300

    fixed nordling not getting candledark recipes
    settlementdecor door crafting time balanced

--- server.lua
+++ server.lua
@@ -1,5 +1,5 @@
 mod_repairer = {}
-print("Mod Repairer version 20.8.25")
+print("Mod Repairer version 20.9.2")
 
 --[[
 todo:
@@ -9,6 +9,7 @@ veloren_elves mod
 port taiga seeds to norsehearth
 
 settlement decor making things float weird :/
+door crafting time
 
 ]]
 
